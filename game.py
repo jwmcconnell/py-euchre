9c84ce1e98b757dda439aeeb73849ff24d0516a1
Jack McConnell <jackmc1122@gmail.com>
2019-10-15 13:13:32 -0700

    Add game file.

---
+++ game.py
@@ -0,0 +1,12 @@
+class Game: 
+
+  def __init__(self):
+    self.game_menu()
+
+  def game_menu(self):
+    print("Hi! Thank you for starting a game of py-euchre")
+    return
+
+
+if __name__ == "__main__":
+  game = Game()
