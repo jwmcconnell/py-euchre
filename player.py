78b6ae75d5074fd9c411e7d36413e29f3cd999f3
Jackmc <jackmc1122@gmail.com>
2019-10-15 19:24:55 -0700

    Send each player a decision during the first round of picking trump

--- player.py
+++ player.py
@@ -9,3 +9,13 @@ class Player:
   def display_player_info(self):
     print('Player', self.position)
     self.hand.display_hand()
+
+  def trump_choice(self, pos_trump):
+    self.hand.display_hand()
+    print("Would you like", pos_trump.suit, "as trump?")
+    while True:
+      decision = input("[yes]/[no]: ")
+      if decision == "yes" or decision == "y":
+        return True
+      elif decision == "no" or decision == "n":
+        return False
--- table.py
+++ table.py
@@ -27,9 +27,19 @@ class Table:
       i += 5
     self.kitty = self.deck.cards[20:]
     self.display_table()
+    self.pick_trump()
+
+  def pick_trump(self):
+    self.first_trump_choice()
+      
+  def first_trump_choice(self):
+    ordered_table = self.order_table()
+    for player in ordered_table:
+      player.trump_choice(self.kitty[0])
 
   def display_table(self):
     for player in self.players:
       player.display_player_info()
     print("Top Card from Kitty:")
     self.kitty[0].print_card()
+    print()
