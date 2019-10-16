5a3820f62d671890339716f3907ce0b466d57646
Jackmc <jackmc1122@gmail.com>
2019-10-15 18:43:16 -0700

    Fix spacing

--- hand.py
+++ hand.py
@@ -2,22 +2,22 @@ from card import Card, Bower
 
 class Hand:
 
-    def __init__(self):
-        self.cards = []
+  def __init__(self):
+    self.cards = []
 
-    def add_card(self, card):
-        self.cards.append(card)
+  def add_card(self, card):
+    self.cards.append(card)
 
-    def remove_card(self, card):
-        self.cards.remove(card)
+  def remove_card(self, card):
+    self.cards.remove(card)
 
-    def add_cards(self, cards):
-        for card in cards:
-            self.cards.append(card)
+  def add_cards(self, cards):
+    for card in cards:
+      self.cards.append(card)
 
-    def display_hand(self):
-        print("Hand:", end="\n")
-        for i, card in enumerate(self.cards):
-            print(i, ": ", sep="", end="")
-            card.print_card()
-        print("")
+  def display_hand(self):
+    print("Hand:", end="\n")
+    for i, card in enumerate(self.cards):
+      print(i, ": ", sep="", end="")
+      card.print_card()
+    print("")
